94a272e3f4035879e28288c374df22188e466adc
Bruno Silva Carvalho <47616928+brunocarvalhs@users.noreply.github.com>
2023-04-05 15:18:27 -0300

    feat: rename theme of submodule

--- android-compose/templates/features/home/src/main/kotlin/{{inputs.package_name}}.home/ui/HomeScreen.kt
+++ android-compose/templates/features/home/src/main/kotlin/{{inputs.package_name}}.home/ui/HomeScreen.kt
@@ -3,7 +3,7 @@ package {{inputs.package_name}}.home.ui
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import {{inputs.package_name}}.commons.theme.MyApplicationTheme
+import {{inputs.package_name}}.commons.theme.{{app_name}}Theme
 
 
 @Composable
@@ -14,7 +14,7 @@ fun Greeting(name: String) {
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MyApplicationTheme {
+    {{app_name}}Theme {
         Greeting("Android")
     }
 }
